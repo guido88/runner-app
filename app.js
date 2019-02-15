9e86eb3ad9c312043118c1d5c49fc91273877070
guido88 <guido211@hotmail.it>
2019-02-15 17:55:56 +0000

    Major improvements,some check needed, style missing

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ class Clock {
   _render() {
     let datet = new Date();
 
-    let hours = datet.getHours()-this._date.getHours();
+    let hours = ((datet.getHours()-this._date.getHours())+60)%60;
     if (hours < 10) hours = '0' + hours;
 
-    let mins = datet.getMinutes()-this._date.getMinutes();
+    let mins = ((datet.getMinutes()-this._date.getMinutes())+60)%60;
     if (mins < 10) mins = '0' + mins;
 
-    let secs = datet.getSeconds()-this._date.getSeconds();
+    let secs = ((datet.getSeconds()-this._date.getSeconds())+60)%60;
     if (secs < 10) secs = '0' + secs;
 
     let output = this._template
@@ -61,144 +61,272 @@ const timer = document.querySelector("#timer");
 const table = document.querySelector("#table");
 
 let clock = new Clock({ template: 'h:m:s' });
-
-let st1= new Date(0);
-console.log(st1.getHours()+":"+st1.getMinutes()+":"+st1.getSeconds())
-
-let st3= new Date(3600*1000);
-console.log(st3.getHours()+":"+st3.getMinutes()+":"+st3.getSeconds())
-
-//console.log(st3.getHours()-st1.getHours());
-//average(st3-st1,3);
-
-let av_date = new Date(3600*1000);
-   
-alert(av_date.getHours());  
-console.log(av_date.getHours()+":"+av_date.getMinutes()+":"+av_date.getSeconds())
-   
-function average(ov_time,lap_count){
-
-    
-    let av_date = new Date(3600*1000);
-   
-    
-    console.log(av_date.getHours()+":"+av_date.getMinutes()+":"+av_date.getSeconds())
-   
-   }
-
-
-
-addName.addEventListener("click", () => {
-
-		createRow(table);
-
-});
-
-
-
-startRace.addEventListener("click", () => {
-
-	
-
-    clock.start();
-
-});
-
-
-endRace.addEventListener("click", () => {
-
-		clock.stop();
-
-});
-
+let mapRunners = new Map();
 
 
 const createRow = (table) => {
 
-		let newRow = document.createElement("tr");
-		newRow.setAttribute('id',inputName.value);
+    if(!inputName.value)
+      return;
 
-		let tdName = document.createElement("td");
-		let tdLap = document.createElement("td");
-		let tdTotal = document.createElement("td");
-		let tdAverage = document.createElement("td");
-		let tdLast = document.createElement("td");
+    if(mapRunners.get(inputName.value))
+      return;
 
-		let button = document.createElement("button");
-		button.innerHTML= inputName.value;
+    let newRow = document.createElement("tr");
+    newRow.setAttribute('id',inputName.value);
 
-		tdName.appendChild(button);
-        tdLap.innerHTML= 0;
-        tdTotal.innerHTML= 0;
-		tdAverage.innerHTML= 0;
-		tdLast.innerHTML= 0;
+    let tdName = document.createElement("td");
+    let tdLap = document.createElement("td");
+    let tdTotal = document.createElement("td");
+    let tdAverage = document.createElement("td");
+    let tdLast = document.createElement("td");
+
+    let button = document.createElement("button");
+    button.innerHTML= inputName.value;
+
+    tdName.appendChild(button);
+    tdLap.innerHTML= 0;
+    tdTotal.innerHTML= 0;
+    tdAverage.innerHTML= 0;
+    tdLast.innerHTML= 0;
        
-		newRow.appendChild(tdName);
-		newRow.appendChild(tdLap);
-		newRow.appendChild(tdTotal);
-		newRow.appendChild(tdAverage);
-		newRow.appendChild(tdLast);
+    let runner = {count:0,total:0,avg:0,current:0,last:0,fast:0};
+    mapRunners.set(inputName.value,runner);
+
+    newRow.appendChild(tdName);
+    newRow.appendChild(tdLap);
+    newRow.appendChild(tdTotal);
+    newRow.appendChild(tdAverage);
+    newRow.appendChild(tdLast);
 
 
-		button.addEventListener("click", () => {
+    button.addEventListener("click", () => {
 
-		     clickName(newRow);
+         clickName(newRow);
 
-		});
+    });
 
 
-		table.appendChild(newRow);
+    table.appendChild(newRow);
 
 };
 
 const clickName = (row) => {
 
-    let lapCount =row.querySelectorAll('td')[1];
-	  lapCount.innerHTML= Number(lapCount.textContent) +1;
+    if(clock.isStarted()){
+    
+    let newDate = new Date();
 
-    ////....
+    let tdName = row.querySelectorAll('td')[0];
+    let b = row.querySelector('button');
+    let name = b.textContent;
+
+    let runner = mapRunners.get(name);
+
+    let lapCount =row.querySelectorAll('td')[1];
+    let laps= Number(lapCount.textContent) +1;
+    lapCount.innerHTML= laps;
+    runner.count=laps;
+
+    let tdTotal =row.querySelectorAll('td')[2];
+    let total = totalTime(clock.getDate(),newDate);
+    tdTotal.innerHTML=total;
+    runner.total=total;
+
+    let tdAvg =row.querySelectorAll('td')[3];
+    let avg = average(clock.getDate(),newDate,laps);
+    tdAvg.innerHTML=avg;
+    runner.avg=avg;
+
+    //REVISION
+    let tdLast =row.querySelectorAll('td')[4];
+    let last = calcLastLap(newDate-runner.current);
+    tdLast.innerHTML=last;
+    runner.last=last;
+
+    runner.current = newDate;
+
+    calcFastestLap(mapRunners,name,last);
+
+    console.log(mapRunners);
+  }
 };
 
-/*  ONCE clicked START RACE
+function calcLastLap(last_lap){
 
-   -each time click NAME runner
-   - store the last lap (time-on-LAST-CLICK - prevOveralTime)
-   - Overal time (time passed from START until LAST CLICK)
-   - Average (Overall time/ num laps)
-   - 
+ 
+    let hours = (Math.floor(last_lap/(3600*1000)))%60;
+    let mins = (Math.floor(last_lap/(60*1000)))%60;
+    let secs = (Math.floor(last_lap/1000))%60;
 
-  ONCE clicked END RACE
-    - Runner with HIGHEST LAP Count and LOWEST OVERALL TIME
-    - FASTEST LAP and TIME taken for it
+    if(hours<10)
+      hours= '0'+hours;
+    if(mins<10)
+      mins='0'+mins;
+    if(secs<10)
+      secs='0'+secs;
 
+    return hours+":"+mins+":"+secs;
 
- A Map should store for each runner currentOverallTime and fastest lap
+}
 
-*/
+function average(start,end,lap_count){
 
+    let diff= (end-start)/lap_count;
+    console.log(diff);
+    let hours = (Math.floor(diff/(3600*1000)))%60;
+    let mins = (Math.floor(diff/(60*1000)))%60;
+    let secs = (Math.floor(diff/1000))%60;
 
+    if(hours<10)
+      hours= '0'+hours;
+    if(mins<10)
+      mins='0'+mins;
+    if(secs<10)
+      secs='0'+secs;
 
+    return hours+":"+mins+":"+secs;
+   
+}
 
+function totalTime(start,end){
 
+    let diff= (end-start)
+    console.log(diff);
+    let hours = (Math.floor(diff/(3600*1000)))%60;
+    let mins = (Math.floor(diff/(60*1000)))%60;
+    let secs = (Math.floor(diff/1000))%60;
 
-function calcFastestLap(f_laps){
+    if(hours<10)
+      hours= '0'+hours;
+    if(mins<10)
+      mins='0'+mins;
+    if(secs<10)
+      secs='0'+secs;
 
+    return hours+":"+mins+":"+secs;
+   
+}
+
+function cleanTable(table){
+
+  let rows = table.querySelectorAll('tr');
+
+  for(let i = 1; i < rows.length; i++){
+    
+    rows[i].remove();
+  
+  }
+
+}
+
+//REVISION
+function calcFastestLap(map,name,last){
+
+  let runner = map.get(name);
+  let c_fast = runner.fast;
+
+  if(c_fast==0)
+    runner.fast=last;
+  else if(last<c_fast){
+    runner.fast=last;
+  }
+
+}
+
+function initializeCurrents(map){
+
+  let values = map.values();
+
+  for (let v of values) {
+    // statement
+      v.current=clock.getDate();
+  }
+
+}
+
+//REVISION
+function calcFinalStats(map){
+
+    let maxCount=0;
+    let minTime =0;
+    let fastest =0;
+
+    let name_fast_lap='';
+    let name_runner='';
+
+    let keys = map.keys();
+
+    for (k of keys) {
+
+      let c_count= map.get(k).count;
+      let c_total= map.get(k).total;
+      let c_fast = map.get(k).fast;
+
+      if(maxCount==0 && minTime==0){
+        maxCount=c_count;
+        minTime = c_total;
+        name_runner=k;
+      }
+      else if(c_count>maxCount && c_total<minTime){
+        maxCount=c_count;
+        minTime = c_total;
+        name_runner=k;
+      }
+      
+      if(fastest==0){
+        name_fast_lap=k;
+        fastest=c_fast;
+      }
+      else if(c_fast<fastest){
+
+          fastest=c_fast;
+          name_fast_lap=k;
+
+      }
+    }
+
+    let span1 = document.createElement("span");
+    span1.innerHTML="The max count is: "+maxCount+" and time: "+minTime+" "+name_runner;
+    let span2 = document.createElement("span");
+    span2.innerHTML="The fastest lap is: "+fastest+" and runner is : "+name_fast_lap;
+
+    span1.setAttribute("id",1);
+    span2.setAttribute("id",2);
+    document.body.appendChild(span1);
+    document.body.appendChild(document.createElement("br"));
+    document.body.appendChild(span2);
 
 }
 
 
-function calcLastLap(now,prev_ov_time){
+addName.addEventListener("click", () => {
 
- //updateFastLap,if
+ if(!clock.isStarted()){
+    createRow(table);
+  }
+});
 
-}
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+startRace.addEventListener("click", () => {
 
-async function demo() {
-  console.log('Taking a break...');
-  await sleep(2000);
-  console.log('Two seconds later');
-}
+    let node1 = document.getElementById("1");
+    let node2 = document.getElementById("2");
+
+    if(node1)
+      node1.remove();
+    if(node2)
+      node2.remove();
+  
+    clock.start();
+    initializeCurrents(mapRunners);
+});
+
+
+endRace.addEventListener("click", () => {
+
+    clock.stop();
+    calcFinalStats(mapRunners);
+    cleanTable(table);
+    mapRunners.clear();
+});
